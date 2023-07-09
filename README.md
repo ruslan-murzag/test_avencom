1451c435e82ea550c7356e72c805e1a1d2c45ad8
ruslan <rus.mur2001@gmail.com>
2023-07-09 15:28:37 +0500

    Add some files and finish with sort and clear data

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 1. First run db.py file in order to connect to db(write your data in the file)
 2. Then run main.py in will take and clear the data from data.csv
 3. In query.txt You can find sql sort function.
+4. telegram_sender file have call-back function to /start command and send the document to the user.
+## Problem of the code
+1. The clear data don't work correctly. You can see that in db when you sort the data.
+2. In sql function don't have report for the auto year with max popular.
--- telegram_sender.py
+++ telegram_sender.py
@@ -1,28 +1,45 @@
 from db import connect_sql
 import pandas as pd
 import telebot
-
-conn, cursor = connect_sql('db_avencom', 'user_avencom', '', 'localhost')
-cursor.callproc('auto_report')
-
-result = cursor.fetchall()
-df = pd.DataFrame(result, columns=['Brand', 'Model', 'Year', 'AVG Price', 'Min Price', 'Max Price', 'Amount'])
-df.to_excel('output.xlsx')
-
-conn.commit()
-
-cursor.close()
-conn.close()
+from telebot import types
 
 Token = ''
 bot = telebot.TeleBot(Token)
 
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    doc = open('output.xlsx', 'rb')
+def make_xlsx():
+    conn, cursor = connect_sql('db_avencom', 'user_avencom', '', 'localhost')
+    cursor.callproc('auto_report')
 
-    bot.send_document(message, doc)
+    result = cursor.fetchall()
+    df = pd.DataFrame(result, columns=['Brand', 'Model', 'Year', 'AVG Price', 'Min Price', 'Max Price', 'Amount'])
+    df.to_excel('output.xlsx')
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+@bot.message_handler(commands=['start'])
+def url(message):
+    make_xlsx()
+    file = open('output.xlsx', 'rb')
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup = markup)
+    bot.send_document(message.chat.id, document=file)
+    print(message.chat.id)
+
+
+def send_rep_users():
+    ids = ['545620069']
+    file = open('output.xlsx', 'rb')
+    for id in ids:
+        bot.send_document(id, document=file)
+
 
 
 bot.infinity_polling()
+
